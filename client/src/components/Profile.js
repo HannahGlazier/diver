d9dd4c15d2cfbd730748a4a8f40feb4c19f8ec4e
Hannah Glazier <hrayglazier@aol.com>
2022-05-02 16:15:09 -0600

    minimal profile updates

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import Typography from '@mui/material/Typography';
+import CardHeader from '@mui/material/CardHeader';
+import Avatar from '@mui/material/Avatar';
+
 function Profile({ user }) {
 
     const follower = user.followers
@@ -10,9 +14,11 @@ function Profile({ user }) {
 
 
     return (
-        <div>
+        <div className="profile">
             <h1>Profile Page</h1>
             <h2>{user.name}</h2>
+            <h2>Homebase: {user.homebase}</h2>
+            <h2>Certification Level: {user.certification_level} / {user.certification_date}</h2>
 
             <h3>Followers</h3>
             <h4>{followerMap}</h4>

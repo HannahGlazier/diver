30f1a93307da046ed421c27c9e1c7a69601cadd8
Hannah Glazier <hrayglazier@aol.com>
2022-06-06 10:07:13 -0600

    Cleaned up Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,7 +7,7 @@ import { useHistory } from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Box, Toolbar, IconButton, Typography, Menu, Button, Tooltip, MenuItem, Avatar, Container, ThemeProvider} from '@mui/material';
 
-function Header({user, setUser, handleLogoutClick, theme}) {
+function Header({user, handleLogoutClick, theme}) {
 
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
@@ -181,62 +181,3 @@ function Header({user, setUser, handleLogoutClick, theme}) {
 };
 
 export default Header;
-
-
-
-
-
-
-
-// import React from 'react'
-// import { NavLink } from "react-router-dom";
-
-// function Header({ handleLogoutClick }) {
-//   return (
-//     <div >
-//       <div className="ui secondary pointing menu">
-//         <NavLink to="/" className="item">
-//           Home
-//         </NavLink>
-//         <NavLink to="/addLog" className="item">
-//           Add Dive Log
-//         </NavLink>
-//         <NavLink to="/addSite" className="item">
-//           Add Dive Site
-//         </NavLink>
-//         <NavLink to="/profile" className="item">
-//           Profile
-//         </NavLink>
-//         <div className="right menu">
-//           <button 
-//             className="ui item"
-//             onClick={handleLogoutClick}
-//           >
-//             Logout
-//           </button>
-//         </div>
-//       </div>
-//       <div className="ui segment">
-//         <p></p>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Header
-
-// import React from 'react'
-// import { NavLink } from "react-router-dom";
-
-// function Header() {
-//   return (
-//     <div className="header">Header
-//       <NavLink to="/">Home</NavLink>
-//       <NavLink to="/addLog">Add Dive Log</NavLink>
-//       <NavLink to="/addSite">Add Dive Site</NavLink>
-//       <NavLink to="/profile">Profile Page</NavLink>
-//     </div>
-//   )
-// }
-
-// export default Header
